c6ea407bd8e600d857c79989fc0ce221728d007a
Kamal Ghimire <kamalghimire121@gmail.com>
2021-06-06 18:09:52 -0400

    routing source file for css, javaScript

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/404_Error')
-def error_page():
-    return render_template('404.html')
-
-
 @app.route('/about')
 def about_page():
     return render_template('about.html')
@@ -21,9 +16,9 @@ def contact_page():
     return render_template('contact.html')
 
 
-@app.route('/portfolio')
-def portfolio_page():
-    return render_template('portfolio.html')    
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')    
 
 
 
